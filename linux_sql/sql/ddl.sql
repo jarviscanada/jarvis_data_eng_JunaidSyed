bf8554a044731e09699766134ca3890a4bcf3642
Junaid Syed <junaidsyedali2000@gmail.com>
2022-12-01 21:16:56 +0000

    changed timestamp column  name

--- linux_sql/sql/ddl.sql
+++ linux_sql/sql/ddl.sql
@@ -10,18 +10,18 @@ CREATE TABLE IF NOT EXISTS host_info (
     cpu_mhz FLOAT NOT NULL,
     L2_cache INTEGER NOT NULL,
     total_mem INTEGER NOT NULL,
-    "timestamp" TIMESTAMP NOT NULL
+    timestamp TIMESTAMP NOT NULL
 );
 
 -- host_usage table
 CREATE TABLE IF NOT EXISTS host_usage (
-    "timestamp" TIMESTAMP NOT NULL,
+    timestamp TIMESTAMP NOT NULL,
     host_id INTEGER NOT NULL,
     memory_free INTEGER NOT NULL,
     cpu_idle INTEGER NOT NULL,
     cpu_kernel INTEGER NOT NULL,
     disk_io INTEGER NOT NULL,
     disk_available INTEGER NOT NULL,
-    PRIMARY KEY (timestampUTC, host_id),
+    PRIMARY KEY (timestamp, host_id),
     FOREIGN KEY (host_ID) REFERENCES host_info (id) ON DELETE CASCADE
 );
